073eb3d33c5e337f46d599830137789beb4d450e
Igor Nascimento <141491741+igor-nasciment0@users.noreply.github.com>
2026-09-02 17:25:52 +0000

    semana 3 - inicio

---
+++ semana3/sum-of-two.cpp
@@ -0,0 +1,39 @@
+// 
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+int main()
+{
+    ll n, x;
+
+    cin >> n >> x;
+    
+    map<ll, ll> pos;
+    
+    pair<ll, ll> result = {-1, -1};
+
+    for(ll i = 0; i < n; i++)
+    {
+        ll a;
+        cin >> a;
+    
+        if (pos.count(x - a) == 1)
+        {
+            result = {i + 1, pos[x - a] + 1};
+            break;
+        }
+
+        pos.insert({a, i});
+    }
+
+    if (result.first == -1 && result.second == -1)
+        cout << "IMPOSSIBLE";
+    else
+        cout << result.first << " " << result.second;    
+
+    return 0;
+}
